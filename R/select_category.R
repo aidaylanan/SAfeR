8d1c21146537cea959ce24ef16d1488cc6fb6f38
Jason Chong <jasonchongg@gmail.com>
2017-07-03 06:43:03 -0700

    Fixed NULL error with create_data_frame

--- R/select_category.R
+++ R/select_category.R
@@ -6,7 +6,7 @@ library(reshape2)
 #Private function to create a dataframe of a station's categories
 create_data_frame <- function(x) {
   y <- data.frame (crime[(which(crime$Station == x)), 3])   #create data frame
-  as.vector(y$Category) #converts data frame as vector
+  as.vector(y[,1]) #converts data frame as vector
 }
 
 
