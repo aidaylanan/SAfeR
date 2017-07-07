7349a030a4e31d538c4d6f5260fb70eb860076bc
Jason Chong <jasonchongg@gmail.com>
2017-07-07 14:06:27 -0700

    Fixed indentation issues and syntax

--- R/trend_line.R
+++ R/trend_line.R
@@ -6,17 +6,18 @@
 #' @return A visualization of all the crime rates in a specific Station and Category
 #' @export
 #' @examples
-#' trend_line("Cape Town Central)
+#' trend_line("Cape Town Central")
 #' trend_line("Aberdeen")
 #'
 trend_line <- function(station) {
   s1 <- sprintf("^%s$", station) #to error check for an exact match
   if  (any(grepl(s1, crime[,2])))  {
-  cate <- select_category(station)
-  ggplot((crime[which(crime$Station == station & crime$Category == cate), ]), aes(x=Year, y=Crime_Rate)) + geom_point() + geom_line(group=1)
+    cate <- select_category(station)
+    ggplot((crime[which(crime$Station == station & crime$Category == cate), ]), aes(x=Year, y=Crime_Rate)) +
+      geom_point() +
+      geom_line(group=1)
   }
-else {
-  stop("Please type in valid station.")
-}
-
+  else {
+    stop("Please type in valid station.")
+  }
 }
